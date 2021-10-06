b5e104981434b0a44671cc0d2c69e17da69c99c2
dev-hernandez2 <william@hernandez2.com>
2021-10-05 21:22:32 -0400

    person microservice

--- modules/person_service/app/udaconnect/controllers.py
+++ modules/person_service/app/udaconnect/controllers.py
@@ -47,3 +47,23 @@ class PersonResource(Resource):
         return person
 
 
+@api.route("/persons/<person_id>/connection")
+@api.param("start_date", "Lower bound of date range", _in="query")
+@api.param("end_date", "Upper bound of date range", _in="query")
+@api.param("distance", "Proximity to a given user in meters", _in="query")
+class ConnectionDataResource(Resource):
+    @responds(schema=ConnectionSchema, many=True)
+    def get(self, person_id) -> ConnectionSchema:
+        start_date: datetime = datetime.strptime(
+            request.args["start_date"], DATE_FORMAT
+        )
+        end_date: datetime = datetime.strptime(request.args["end_date"], DATE_FORMAT)
+        distance: Optional[int] = request.args.get("distance", 5)
+
+        results = ConnectionService.find_contacts(
+            person_id=person_id,
+            start_date=start_date,
+            end_date=end_date,
+            meters=distance,
+        )
+        return results
